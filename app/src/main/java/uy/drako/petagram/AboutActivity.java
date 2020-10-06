27c131d323390098e7ffed704c112eb3d7796185
pablo <pablitouy@gmail.com>
2020-10-06 19:02:49 -0300

    En progreso: Petagram v2

---
+++ app/src/main/java/uy/drako/petagram/AboutActivity.java
@@ -0,0 +1,17 @@
+package uy.drako.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
+    }
+}
